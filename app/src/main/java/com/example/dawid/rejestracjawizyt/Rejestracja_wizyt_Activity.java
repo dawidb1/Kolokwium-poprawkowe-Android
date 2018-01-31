f8e454c07b33097eed8e139e3c920da3d7347c11
dawidb1 <33998058+dawidb1@users.noreply.github.com>
2018-01-31 13:37:03 +0100

    init

---
+++ app/src/main/java/com/example/dawid/rejestracjawizyt/Rejestracja_wizyt_Activity.java
@@ -0,0 +1,13 @@
+package com.example.dawid.rejestracjawizyt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Rejestracja_wizyt_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rejestracja_wizyt_);
+    }
+}
